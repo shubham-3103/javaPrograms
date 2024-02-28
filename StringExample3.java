9cd7614931bd856408ed6d857e7d4c56839a3728
shubham-3103 <73528601+shubham-3103@users.noreply.github.com>
2024-02-28 11:39:03 +0530

    Add files via upload

---
+++ StringExample3.java
@@ -0,0 +1,31 @@
+package com.hughes.pojo;
+/*
+ * Assignment:
+ * firstname
+ * lastname
+ * display(){return firstname+lastname;}
+ * */
+public class StringExample3 {
+
+	
+	private String firstName;
+	private int salary;
+	public StringExample3() {}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
